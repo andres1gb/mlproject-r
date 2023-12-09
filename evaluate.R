75d8a6d0a16074593414dbf23f9013b0056ae7ba
Andrés Purriños <andres1gb@gmail.com>
2023-12-09 12:52:18 +0100

    Add R2 adjusted

--- evaluate.R
+++ evaluate.R
@@ -1,6 +1,12 @@
 print(describe(results[-3]))
 print("R2:")
-print(R2(results$prediction, results$actual, na.rm = TRUE))
+r2 = R2(results$prediction, results$actual, na.rm = TRUE)
+print(r2)
+print("R2 adjusted:")
+n = nrow(testset)
+k = last_dimension - 2
+r2corr = 1-((1-r2)*(n-1)/(n-k-1))
+print(r2corr)
 print("RMSE:")
 print(RMSE(results$prediction, results$actual, na.rm = TRUE))
 View(results)
